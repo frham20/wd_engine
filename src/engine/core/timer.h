3067f2c037b64a92ac1fabb6817b5f0478b03bd2
Francois Hamel <francois.hamel@gmail.com>
2017-01-06 16:55:40 -0500

    types and platform setup

---
+++ src/engine/core/timer.h
@@ -0,0 +1,12 @@
+#pragma once
+namespace wd
+{
+	class timer
+	{
+	public:
+		timer();
+
+	private:
+
+	};
+}
--- src/engine/wd.h
+++ src/engine/wd.h
@@ -1,5 +1,14 @@
 #pragma once
-#include <windows.h>
+
+#if defined(WD_PLATFORM_WINDOWS)
+	#include "wd_windows.h"
+#else
+	#error Unsupported platform!
+#endif
 
 //Acceptable warnings
 #pragma warning(disable: 4100) //warning C4100: 'xxxxx': unreferenced formal parameter
+
+
+#include "core\types.h"
+#include "core\timer.h"
