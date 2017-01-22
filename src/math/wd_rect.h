0d6af43f37c0f0be4e09fb4378be1a2fc4a243bf
Francois Hamel <francois.hamel@gmail.com>
2017-01-22 17:09:58 -0500

    add typedefs for base types for rect<T> and set default constructor to default imp

--- src/math/wd_rect.h
+++ src/math/wd_rect.h
@@ -5,7 +5,7 @@ template<typename T>
 class rect
 {
 public:
-	rect();
+	rect() = default;
 	rect(const rect<T>& other) = default;
 	rect(rect<T>&& other) = default;
 	rect(T left, T top, T right, T bottom);
@@ -36,6 +36,20 @@ public:
 
 template<typename T> T area(const rect<T>& r);
 template<typename T> T perimeter(const rect<T>& r);
+
+
+typedef rect<int8>  recti8;
+typedef rect<int16> recti16;
+typedef rect<int32> recti;
+typedef rect<int64> recti64;
+
+typedef rect<uint8>  rectui8;
+typedef rect<uint16> rectui16;
+typedef rect<uint32> rectui;
+typedef rect<uint64> rectui64;
+
+typedef rect<float>  rectf;
+typedef rect<double> rectd;
 
 }
 
